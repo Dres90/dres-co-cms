4bbd584b6e9c4426fde8a89e71aec122ce17833a
Andrés Cárdenas <acardenas90@gmail.com>
2023-03-01 15:59:51 +0100

    Create new route for controller

--- src/plugins/recipe-loader/server/controllers/recipe-job-controller.js
+++ src/plugins/recipe-loader/server/controllers/recipe-job-controller.js
@@ -7,7 +7,7 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('plugin::recipe-loader.recipe-job', {
-    async create(ctx) {
+    async run(ctx) {
         const data = await super.create(ctx);
         return data;
     }
